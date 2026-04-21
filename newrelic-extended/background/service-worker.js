28be229d3db5edf6bbd3129da9ec067af98ecfed
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2026-04-21 18:20:21 +0300

    fix: open options

--- newrelic-extended/background/service-worker.js
+++ newrelic-extended/background/service-worker.js
@@ -4,6 +4,9 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "OPEN_OPTIONS") {
-    chrome.runtime.openOptionsPage();
+    chrome.runtime.openOptionsPage().catch((err) => {
+      console.warn("openOptionsPage failed, falling back to tab:", err);
+      chrome.tabs.create({ url: chrome.runtime.getURL("options/options.html") });
+    });
   }
 });
